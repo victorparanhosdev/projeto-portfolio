2728546c70261a2db2f9d5cc85b701d6b99b86fc
Victor Paranhos <victor_paranhos@hotmail.com>
2022-03-16 14:19:31 -0300

    UP

--- funcao.js
+++ funcao.js
@@ -1,14 +1,11 @@
-const menu = document.querySelector(".menu-mobile")
+const menu = document.querySelectorAll(".menu-mobile")
 const menuHamburguer = document.querySelector(".menu-hamburguer")
-show = true
+
 menuHamburguer.addEventListener('click', ()=> {
-    if(show) {
-        menu.classList.add('show')
-        show = false
-    } else{
-        menu.classList.remove('show')
-        show = true
+    
+    for(let element of menu){
+    element.classList.toggle('show')
     }
     
-
 })
+
