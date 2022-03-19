f800f241fd6354c75a9f240d95f31aec058647e2
Victor Paranhos <victor_paranhos@hotmail.com>
2022-03-19 00:17:52 -0300

    Atualizando

--- funcao.js
+++ funcao.js
@@ -10,13 +10,35 @@ menuHamburguer.addEventListener('click', ()=> {
     element.classList.toggle('show')
     
     }
+    esconder() 
 
+})
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
 })
 
 for (const link of menuShow){  
     link.addEventListener('click', ()=> {
       menumobile.classList.remove('show')
-     
+      esconder() 
     })
+
 }
 
+function esconder() {
+  if(menumobile.classList.contains('show')){
+    document.body.style.overflow = 'hidden'
+  } else {
+    document.body.style.overflow = 'initial'
+  }
+}
